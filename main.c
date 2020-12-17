78c9cda631cd008f2a7ee96ac7827dd5f6143413
MSathieu <18145111+MSathieu@users.noreply.github.com>
2020-12-17 09:03:40 +0100

    Give processes correct capabilities

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <capability.h>
 #include <spawn.h>
 #include <unistd.h>
 
@@ -6,8 +7,10 @@ int main(void) {
     return 1;
   }
   spawn_process("ipcd");
+  grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_GET_CAPS);
   start_process();
   spawn_process("kbdd");
+  grant_capability(CAP_NAMESPACE_IPCD, CAP_IPCD_REGISTER);
   start_process();
   spawn_process("ps2d");
   grant_ioport(0x60);
