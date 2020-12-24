b33e138032085882dec576976dd11637b27695c8
MSathieu <18145111+MSathieu@users.noreply.github.com>
2020-12-24 10:26:03 +0100

    Grant additional capabilities

--- main.c
+++ main.c
@@ -11,8 +11,9 @@ int main(void) {
   grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_GET_CAPS);
   start_process();
   spawn_process("fbd");
-  grant_capability(CAP_NAMESPACE_SERVERS, CAP_IPCD_REGISTER);
+  grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_GET_CAPS);
   grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_GET_FB_INFO);
+  grant_capability(CAP_NAMESPACE_SERVERS, CAP_IPCD_REGISTER);
   uintptr_t fb_phys_addr = _syscall(_SYSCALL_GET_FB_INFO, 0, 0, 0, 0, 0);
   size_t height = _syscall(_SYSCALL_GET_FB_INFO, 2, 0, 0, 0, 0);
   size_t pitch = _syscall(_SYSCALL_GET_FB_INFO, 3, 0, 0, 0, 0);
@@ -29,6 +30,7 @@ int main(void) {
   register_irq(1);
   start_process();
   spawn_process("ttyd");
-  grant_capability(CAP_NAMESPACE_DRIVERS, CAP_FBD_WRITE);
+  grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_GET_FB_INFO);
+  grant_capability(CAP_NAMESPACE_DRIVERS, CAP_FBD_DRAW);
   start_process();
 }
