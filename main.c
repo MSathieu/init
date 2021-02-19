c5d016fe8514db0fd8250a09fa7054691877307b
MSathieu <18145111+MSathieu@users.noreply.github.com>
2021-02-19 11:17:02 +0100

    Use absolute paths

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ struct process {
   pid_t pid;
 };
 
-static struct process processes[] = {{"argd", 1, 0}, {"atad", 1, 0}, {"ipcd", 1, 0}, {"sbin/envd", 0, 0}, {"sbin/fbd", 0, 0}, {"sbin/kbdd", 0, 0}, {"sbin/ps2d", 0, 0}, {"sbin/ttyd", 0, 0}, {"vfsd", 1, 0}};
+static struct process processes[] = {{"argd", 1, 0}, {"atad", 1, 0}, {"ipcd", 1, 0}, {"/sbin/envd", 0, 0}, {"/sbin/fbd", 0, 0}, {"/sbin/kbdd", 0, 0}, {"/sbin/ps2d", 0, 0}, {"/sbin/ttyd", 0, 0}, {"vfsd", 1, 0}};
 
 static void spawn(const char* name) {
   for (size_t i = 0; i < sizeof(processes) / sizeof(struct process); i++) {
@@ -37,24 +37,24 @@ static void spawn(const char* name) {
     register_irq(14);
     register_irq(15);
     grant_capability(CAP_NAMESPACE_FILESYSTEMS, CAP_VFSD_SVFS);
-  } else if (!strcmp(name, "sbin/envd")) {
+  } else if (!strcmp(name, "/sbin/envd")) {
     grant_capability(CAP_NAMESPACE_SERVERS, CAP_IPCD_REGISTER);
-  } else if (!strcmp(name, "sbin/fbd")) {
+  } else if (!strcmp(name, "/sbin/fbd")) {
     grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_GET_FB_INFO);
     grant_capability(CAP_NAMESPACE_SERVERS, CAP_IPCD_REGISTER);
     uintptr_t fb_phys_addr = _syscall(_SYSCALL_GET_FB_INFO, 0, 0, 0, 0, 0);
     size_t height = _syscall(_SYSCALL_GET_FB_INFO, 2, 0, 0, 0, 0);
     size_t pitch = _syscall(_SYSCALL_GET_FB_INFO, 3, 0, 0, 0, 0);
     map_physical_memory(fb_phys_addr, height * pitch);
-  } else if (!strcmp(name, "sbin/kbdd")) {
+  } else if (!strcmp(name, "/sbin/kbdd")) {
     grant_capability(CAP_NAMESPACE_SERVERS, CAP_IPCD_REGISTER);
     grant_capability(CAP_NAMESPACE_SERVERS, CAP_KBDD);
-  } else if (!strcmp(name, "sbin/ps2d")) {
+  } else if (!strcmp(name, "/sbin/ps2d")) {
     grant_capability(CAP_NAMESPACE_SERVERS, CAP_KBDD_SEND_KEYPRESS);
     grant_ioport(0x60);
     grant_ioport(0x64);
     register_irq(1);
-  } else if (!strcmp(name, "sbin/ttyd")) {
+  } else if (!strcmp(name, "/sbin/ttyd")) {
     grant_capability(CAP_NAMESPACE_DRIVERS, CAP_FBD_DRAW);
     grant_capability(CAP_NAMESPACE_SERVERS, CAP_IPCD_REGISTER);
     grant_capability(CAP_NAMESPACE_SERVERS, CAP_KBDD_RECEIVE_EVENTS);
@@ -71,11 +71,11 @@ int main(void) {
   spawn("argd");
   spawn("vfsd");
   spawn("atad");
-  spawn("sbin/envd");
-  spawn("sbin/fbd");
-  spawn("sbin/kbdd");
-  spawn("sbin/ps2d");
-  spawn("sbin/ttyd");
+  spawn("/sbin/envd");
+  spawn("/sbin/fbd");
+  spawn("/sbin/kbdd");
+  spawn("/sbin/ps2d");
+  spawn("/sbin/ttyd");
   while (1) {
     pid_t pid = wait(0);
     for (size_t i = 0; i < sizeof(processes) / sizeof(struct process); i++) {
