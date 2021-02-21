b66981e799e87c9f617750fd9a05abc25479d793
MSathieu <18145111+MSathieu@users.noreply.github.com>
2021-02-21 21:27:03 +0100

    Grant all system processes CAP_KERNEL_PRIORITY

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <__/syscall.h>
 #include <capability.h>
 #include <ipc.h>
+#include <priority.h>
 #include <spawn.h>
 #include <stdbool.h>
 #include <string.h>
@@ -28,7 +29,9 @@ static void spawn(const char* name) {
   }
   if (!strcmp(name, "argd")) {
     register_ipc_name("argd");
+    grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_PRIORITY);
   } else if (!strcmp(name, "atad")) {
+    grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_PRIORITY);
     for (size_t i = 0; i < 8; i++) {
       grant_ioport(0x1f0 + i);
       grant_ioport(0x170 + i);
@@ -38,29 +41,37 @@ static void spawn(const char* name) {
     register_irq(14);
     register_irq(15);
     grant_capability(CAP_NAMESPACE_FILESYSTEMS, CAP_VFSD_SVFS);
+  } else if (!strcmp(name, "ipcd")) {
+    grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_PRIORITY);
   } else if (!strcmp(name, "/sbin/envd")) {
     register_ipc_name("envd");
+    grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_PRIORITY);
   } else if (!strcmp(name, "/sbin/fbd")) {
     register_ipc_name("fbd");
     grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_GET_FB_INFO);
+    grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_PRIORITY);
     uintptr_t fb_phys_addr = _syscall(_SYSCALL_GET_FB_INFO, 0, 0, 0, 0, 0);
     size_t height = _syscall(_SYSCALL_GET_FB_INFO, 2, 0, 0, 0, 0);
     size_t pitch = _syscall(_SYSCALL_GET_FB_INFO, 3, 0, 0, 0, 0);
     map_physical_memory(fb_phys_addr, height * pitch);
   } else if (!strcmp(name, "/sbin/kbdd")) {
     register_ipc_name("kbdd");
+    grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_PRIORITY);
     grant_capability(CAP_NAMESPACE_SERVERS, CAP_KBDD);
   } else if (!strcmp(name, "/sbin/ps2d")) {
+    grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_PRIORITY);
     grant_capability(CAP_NAMESPACE_SERVERS, CAP_KBDD_SEND_KEYPRESS);
     grant_ioport(0x60);
     grant_ioport(0x64);
     register_irq(1);
   } else if (!strcmp(name, "/sbin/ttyd")) {
     register_ipc_name("ttyd");
+    grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_PRIORITY);
     grant_capability(CAP_NAMESPACE_DRIVERS, CAP_FBD_DRAW);
     grant_capability(CAP_NAMESPACE_SERVERS, CAP_KBDD_RECEIVE_EVENTS);
   } else if (!strcmp(name, "vfsd")) {
     register_ipc_name("vfsd");
+    grant_capability(CAP_NAMESPACE_KERNEL, CAP_KERNEL_PRIORITY);
   }
   start_process();
 }
